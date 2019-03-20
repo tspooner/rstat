7c01b74111942cebd996510a70efc808447bae72
Tom Spooner <spooner10000@gmail.com>
2019-03-20 16:39:17 +0000

    refactor(univariate): replace LogNormal with newtype ~ exp(Normal)

--- src/univariate/continuous/lognormal.rs
+++ src/univariate/continuous/lognormal.rs
@@ -1,62 +1,49 @@
 use crate::{
     consts::PI_2,
     core::*,
+    univariate::continuous::Normal,
 };
 use rand::Rng;
 use spaces::{continuous::PositiveReals, Matrix};
 use std::fmt;
 
 #[derive(Debug, Clone, Copy)]
-pub struct LogNormal {
-    pub mu: f64,
-    pub sigma: f64,
-}
+pub struct LogNormal(Normal);
 
 impl LogNormal {
     pub fn new(mu: f64, sigma: f64) -> LogNormal {
-        assert_positive_real!(sigma);
-
-        LogNormal { mu, sigma }
+        LogNormal(Normal::new(mu, sigma))
     }
 
     fn z(&self, x: f64) -> f64 {
-        (x.ln() - self.mu) / self.sigma
+        self.0.z(x.ln())
     }
 }
 
 impl Default for LogNormal {
     fn default() -> LogNormal {
-        LogNormal {
-            mu: 0.0,
-            sigma: 1.0,
-        }
+        LogNormal(Normal::default())
     }
 }
 
 impl Into<rand::distributions::LogNormal> for LogNormal {
     fn into(self) -> rand::distributions::LogNormal {
-        rand::distributions::LogNormal::new(self.mu, self.sigma)
+        rand::distributions::LogNormal::new(self.0.mu, self.0.sigma)
     }
 }
 
 impl Into<rand::distributions::LogNormal> for &LogNormal {
     fn into(self) -> rand::distributions::LogNormal {
-        rand::distributions::LogNormal::new(self.mu, self.sigma)
+        rand::distributions::LogNormal::new(self.0.mu, self.0.sigma)
     }
 }
 
 impl Distribution for LogNormal {
     type Support = PositiveReals;
 
-    fn support(&self) -> PositiveReals {
-        PositiveReals
-    }
+    fn support(&self) -> PositiveReals { PositiveReals }
 
-    fn cdf(&self, x: f64) -> Probability {
-        use special_fun::FloatSpecial;
-
-        (0.5 + (self.z(x) / 2.0f64.sqrt()).erf() / 2.0).into()
-    }
+    fn cdf(&self, x: f64) -> Probability { self.0.cdf(x.ln()) }
 
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> f64 {
         use rand::distributions::{LogNormal as LNSampler, Distribution as DistSampler};
@@ -68,33 +55,28 @@ impl Distribution for LogNormal {
 }
 
 impl ContinuousDistribution for LogNormal {
-    fn pdf(&self, x: f64) -> f64 {
-        let z = self.z(x);
-        let norm = 1.0 / PI_2.sqrt() / self.sigma;
-
-        norm * (-z * z / 2.0).exp() / x
-    }
+    fn pdf(&self, x: f64) -> f64 { self.0.pdf(x.ln()) }
 }
 
 impl UnivariateMoments for LogNormal {
     fn mean(&self) -> f64 {
-        (self.mu + self.sigma * self.sigma / 2.0).exp()
+        (self.0.mu + self.0.sigma * self.0.sigma / 2.0).exp()
     }
 
     fn variance(&self) -> f64 {
-        let sigma2 = self.sigma * self.sigma;
+        let sigma2 = self.0.sigma * self.0.sigma;
 
-        (sigma2.exp() - 1.0) * (2.0 * self.mu + sigma2).exp()
+        (sigma2.exp() - 1.0) * (2.0 * self.0.mu + sigma2).exp()
     }
 
     fn skewness(&self) -> f64 {
-        let sigma2 = self.sigma * self.sigma;
+        let sigma2 = self.0.sigma * self.0.sigma;
 
         (sigma2.exp() + 2.0) * (sigma2.exp() - 1.0).sqrt()
     }
 
     fn excess_kurtosis(&self) -> f64 {
-        let sigma2 = self.sigma * self.sigma;
+        let sigma2 = self.0.sigma * self.0.sigma;
 
         (4.0 * sigma2).exp() + 2.0 * (3.0 * sigma2).exp() + 3.0 * (2.0 * sigma2).exp() - 6.0
     }
@@ -106,25 +88,25 @@ impl Quantiles for LogNormal {
     }
 
     fn median(&self) -> f64 {
-        self.mu.exp()
+        self.0.mu.exp()
     }
 }
 
 impl Modes for LogNormal {
     fn modes(&self) -> Vec<f64> {
-        vec![(self.mu - self.sigma * self.sigma).exp()]
+        vec![(self.0.mu - self.0.sigma * self.0.sigma).exp()]
     }
 }
 
 impl Entropy for LogNormal {
     fn entropy(&self) -> f64 {
-        (self.sigma * (self.mu + 0.5).exp() * PI_2.sqrt()).log2()
+        (self.0.sigma * (self.0.mu + 0.5).exp() * PI_2.sqrt()).log2()
     }
 }
 
 impl FisherInformation for LogNormal {
     fn fisher_information(&self) -> Matrix {
-        let one_over_sigma2 = 1.0 / self.sigma / self.sigma;
+        let one_over_sigma2 = 1.0 / self.0.sigma / self.0.sigma;
 
         unsafe {
             Matrix::from_shape_vec_unchecked(
@@ -142,6 +124,6 @@ impl FisherInformation for LogNormal {
 
 impl fmt::Display for LogNormal {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Lognormal({}, {})", self.mu, self.variance())
+        write!(f, "Lognormal({}, {})", self.0.mu, self.variance())
     }
 }
