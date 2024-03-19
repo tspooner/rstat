9afb4b3f1c9240a58321e2f66abd064e89e41c1f
Banach Technologies <80456992+banachtech@users.noreply.github.com>
2024-03-19 10:17:34 +0800

    Update multivariate.rs
    Covariance matrix in the example should be symmetric.

--- src/normal/multivariate.rs
+++ src/normal/multivariate.rs
@@ -103,7 +103,7 @@ impl<const N: usize> MvNormal<N> {
     /// # use rstat::normal::MvNormal;
     /// let cov = [
     ///     [0.5, 2.0],
-    ///     [1.0, 1.0]
+    ///     [2.0, 1.0]
     /// ];
     /// let dist = MvNormal::new([0.0, 1.0], cov);
     ///
